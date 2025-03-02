910bdab46c092730d81852be2bb5307d0a00a34d
Chris Holden <holdy@users.noreply.github.com>
2025-03-02 12:04:35 +0000

    book 2

--- index.js
+++ index.js
@@ -7,26 +7,42 @@ const queryString =
         : decodeURIComponent(window.location.href.substring(qIndex + 1));
 console.log(`queryString: [${queryString}]`);
 
+const contentIdMatch = queryString.match(/s\d+[\/-]l\d+/i);
+const contentId = contentIdMatch
+    ? contentIdMatch[0].toLowerCase().replace("-", "/")
+    : null;
+
+const itemData = {};
+
 //#region text prep
 
 const prepareText = (text) => {
     const preppedLines = [];
-    for (let line of text.split('\n')){
+    for (let line of text.split("\n")) {
         const testLine = line.trim();
-        if (testLine && testLine[0]==='_' && testLine[testLine.length-1]==='_') {
+        if (
+            testLine &&
+            testLine[0] === "_" &&
+            testLine[testLine.length - 1] === "_"
+        ) {
             // looks line the whole line is a title
-            line = `<h2>${line.replace(/_/g,' ').trim()}</h2>`
+            line = `<h2>${line.replace(/_/g, " ").trim()}</h2>`;
         }
         preppedLines.push(line);
     }
 
-    return preppedLines.join('\n').replace(/\<\/h2\>\n/g,'</h2>').replace(/\n{2}/g, '<br/><br/>');
+    return preppedLines
+        .map((line) => (line.trim() === "" ? "&nbsp;" : line))
+        .join("\n")
+        .replace(/\[\d+\]/g, "") // footnote ref.
+        .replace(/\<\/h2\>\n/g, "</h2>")
+        .replace(/\n/g, "<br/>");
 };
 
 //#endregion text prep
 
 const searchWord = async (word) => {
-    if (!word || (!word.trim())) {
+    if (!word || !word.trim()) {
         return;
     }
 
@@ -34,9 +50,9 @@ const searchWord = async (word) => {
     if (!result.ok) {
         return;
     }
-    const lines = (await result.text()).toString().split('\n');
+    const lines = (await result.text()).toString().split("\n");
     for (let line of lines) {
-        const commentIndex = line.indexOf('//');
+        const commentIndex = line.indexOf("//");
         if (commentIndex !== -1) {
             line = line.substring(0, commentIndex).trim();
         }
@@ -45,12 +61,12 @@ const searchWord = async (word) => {
             console.log(line);
         }
     }
-}
+};
 
 const toggleDisplayed = (id, displayed) => {
     const element = document.getElementById(id);
-    element.style.display = displayed ? 'block' : 'none';
-}
+    element.style.display = displayed ? "block" : "none";
+};
 
 const fillAndShow = (id, htmlContent) => {
     const element = document.getElementById(id);
@@ -58,23 +74,49 @@ const fillAndShow = (id, htmlContent) => {
     element.innerHTML = htmlContent;
 };
 
+const go = (direction) => {
+    const to = itemData[direction];
+    if (to && to != "0") {
+        const url = `.?${contentId.substring(0, 2)}-l${to}.txt`;
+        window.location.href = url;
+    }
+};
+
+const parseDataObject = (text, itemData) => {
+    for (const line of text.split("\n")) {
+        const i = line.indexOf(":");
+        if (i !== -1) {
+            itemData[line.substring(0, i)] = line.substring(i + 1).trim();
+        }
+    }
+};
+
+const nonZero = (text) => text && text != "0";
+const nobr = (text) => text.replace(" ", "&nbsp;");
+
 document.addEventListener("DOMContentLoaded", async () => {
     toggleDisplayed("main-left", false);
     toggleDisplayed("main-right", false);
     toggleDisplayed("main-top", false);
     toggleDisplayed("main-bottom", false);
-    toggleDisplayed("main-header", false);
-    toggleDisplayed("main-footer", false);
     toggleDisplayed("nav-row", false);
 
     if (queryString) {
-        const contentIdMatch = queryString.match(/s\d+[\/-]l\d+/i);
-
-        if (!contentIdMatch) {
+        if (!contentId) {
             return;
         }
-        contentId = contentIdMatch[0].toLowerCase().replace("-", "/");
         console.log("itemId : [" + contentId + "]");
+
+        const itemResult = await fetch(
+            "./data/content/" + contentId + ".index.txt"
+        );
+        if (itemResult.ok) {
+            const text = await itemResult.text();
+            parseDataObject(text, itemData);
+            console.log("ItemData:" + JSON.stringify(itemData));
+        }
+        toggleDisplayed("main-header-end", nonZero(itemData["previous"]));
+        toggleDisplayed("main-footer-end", nonZero(itemData["next"]));
 
         const result = await fetch("./data/content/" + contentId + ".txt");
         if (result.ok) {
@@ -108,8 +150,10 @@ document.addEventListener("DOMContentLoaded", async () => {
                 metadataMap.published
             ) {
                 fillAndShow(
-                    "main-header",
-                    `From '${metadataMap.title}' by ${metadataMap.author} - published ${metadataMap.published}`
+                    "main-header-content",
+                    `From '${nobr(metadataMap.title)}' by ${nobr(
+                        metadataMap.author
+                    )} - published ${metadataMap.published}`
                 );
             }
         }
---
+++ src/indexer.ts
@@ -0,0 +1,74 @@
+// processor.ts chops a source into indexable items (Stories etc)
+// the indexer indexes them an links them together.
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+const PART_FILENAME_PATTERN = /^l\d+\.txt$/;
+
+
+const numberFromFilename = (filename: String): number =>
+    Number(filename.replace("l", "").replace(".txt", ""));
+
+const writeDataObjectFile = (dataObject: any, fileName: string): void => {
+    const lines :string[] = [];
+
+    for (const key of Object.keys(dataObject).sort()) {
+        lines.push(`${key}: ${dataObject[key]}`);
+    }
+
+    fs.writeFileSync(fileName, lines.join('\n'));
+}
+
+/**
+ * Indexes the items in a directory (all part of one book)
+ * The two main outputs are :
+ *  1) An index file per item
+ *  2) Linking if files together. (previous and next links)
+ */
+export function indexItemsIn(directory:string): void {
+    // find the 'l' files
+    const entries = fs.readdirSync(directory).filter(entry => entry.match(PART_FILENAME_PATTERN));
+
+    entries.sort((a,b) => {
+        if (a.match(PART_FILENAME_PATTERN) && b.match(PART_FILENAME_PATTERN)) {
+            const aNum = Number(a.replace('l','').replace('.txt',''));    
+            const bNum = Number(b.replace("l", "").replace(".txt", ""));    
+            return aNum - bNum;
+        }
+        else {
+            return (a < b) ? 1 : -1;
+        }
+    }); 
+
+    const partFiles = entries.filter(entry => entry.match(PART_FILENAME_PATTERN));
+
+    let previousItemNumber = 0;
+    for (let index = 0; index < partFiles.length; index++) {
+            const entry = partFiles[index];
+        
+            const fullFilePath = path.join(directory, entry);
+            console.log("would index: " + fullFilePath);
+
+            const nextFileNumber = (index + 1 < partFiles.length ) ? 
+                numberFromFilename(partFiles[index+1]) : 0;
+            const fileNumber = numberFromFilename(entry);
+            const indexFileName = path.join(
+                directory,
+                `l${fileNumber}.index.txt`
+            );
+
+            const indexData = {
+                previous: previousItemNumber,
+                next: nextFileNumber,
+            };
+
+            writeDataObjectFile(indexData, indexFileName);
+            previousItemNumber = fileNumber;
+    }
+
+
+
+}
+
+indexItemsIn("/Users/Shared/projects/antiqwiki/data/content/s1");;
